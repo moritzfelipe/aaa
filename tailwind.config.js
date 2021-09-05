3a5a1c3b7137d4f9c514237c71a914c3f18ec1aa
moritzfelipe <stellmacher@gmail.com>
2021-09-05 15:19:34 +0200

    -Add dario design -Add new artists

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {    
+        'left': {
+          '0%': { transform: 'translateX(-50%)' },
+          '100%': { transform: 'translateX(-252.1%)' },
+        },     
+      },
+      animation: {
+        'left': 'left 15s linear infinite',  
+        'left-x2': 'left 12s linear infinite', 
+        'left-x3': 'left 8s linear infinite',     
+      },
     },
   },
   variants: {
